0a92e3b2698fb20d6d597a0a61c5a02f519c1ce7
TARXRR <panuwittar@gmail.com>
2024-07-19 16:11:25 +0700

    งาน2

--- EP1/index.ts
+++ EP1/index.ts
@@ -8,3 +8,11 @@ let อยากกลับบ้าน: boolean = false
 console.log(firstName)
 console.log(Y)
 console.log(อยากกลับบ้าน)
+
+const a: number = 1
+const b: number = 2
+
+console.log (a == b)
+ 
+console.log (a > b)
+console.log (a <= b)
