cb2b8c6712cf7b4b73e27adfc4a3e6691a578d1d
TARXRR <panuwittar@gmail.com>
2024-09-05 23:50:30 +0700

    ส่งการบ้าน

---
+++ Homework2/homework.ts
@@ -0,0 +1,14 @@
+const perSoNalInForMaTion  = {
+    fullName: 'Panuwit Kanason',
+    adDress: '115/5' ,
+    swine: '8',
+    road: 'road 66',
+    districts: 'Delaware',
+    canTon: 'New Jersey',
+    ciTy: 'America',
+    zipCode:'152478',
+    email:'Uini1554@gmail.com',
+
+}
+
+console.log(perSoNalInForMaTion)
